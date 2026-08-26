a6dcf287af2a97ab5ed8a2f046063e873406a256
bamou0601 <mo.ba@ves.co.jp>
2026-08-26 17:28:18 +0900

    establish framework and design some testcases

---
+++ AutomationExercise/Common/TestContext.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by Ranorex
+ * User: mo_ba
+ * Date: 2026/08/25
+ * Time: 16:51
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace AutomationExercise
+{
+    /// <summary>
+    /// APIテストとUIテスト間で
+    /// 共有する実行データを管理する。
+    /// </summary>
+	public static class TestContext
+	{	
+		// API検索キーワード
+        public static string SearchProduct { get; set; }
+		
+        // APIで取得した商品名
+        public static string ApiProductName { get; set; }
+		
+        
+		// APIで取得した商品件数
+        public static int ApiProductCount { get; set; }
+        
+        // APIで取得した商品名一覧
+        public static List<string> ApiProductNames { get; set; }
+				
+	}
+}
